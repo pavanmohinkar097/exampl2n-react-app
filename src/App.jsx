bf415ba5f2a6062b13cde41c6d5d173134b45719
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-05-07 13:48:49 +0530

    7-05-2023

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import '/styles/App.css';
+
+const App = () => {
+//code here 
+const[text,setText]=useState("redColor");
+const chng=() =>{
+  if(text == "redColor"){
+    setText("blueColor")
+  }
+  else{
+    setText("redColor");
+  }
+}
+  return (
+    <div id="main">
+      <p className={text} >Newton School</p>
+      <button id='button' onClick={chng}>Change Style</button>
+    </div>
+  )
+}
+
+
+export default App;
